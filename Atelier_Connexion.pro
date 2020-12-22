19b03beea44f2a6a2cf25e52fde5618a15350f8c
MazouzMariem <mariem.mazouz@esprit.tn>
2020-12-22 09:18:23 +0100

    ajout des images

--- Atelier_Connexion.pro
+++ Atelier_Connexion.pro
@@ -53,4 +53,6 @@ qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
-RESOURCES +=
+RESOURCES += \
+    img.qrc \
+    pics1.qrc
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "mainwindow.h"
 #include <QApplication>
 #include <QMessageBox>
 #include "connection.h"
@@ -9,7 +8,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+
 gestionabonne g;
     Connection c;
     bool test=c.createconnect();
