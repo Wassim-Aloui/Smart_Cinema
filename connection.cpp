65320a1f911a2ddf52b843ce0a1fc23fbdc2c2ec
MazouzMariem <mariem.mazouz@esprit.tn>
2021-01-04 21:06:20 +0100

    updatee1

--- connection.cpp
+++ connection.cpp
@@ -9,8 +9,8 @@ bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
 db.setDatabaseName("Source_projet2A");//inserer le nom de la source de données ODBC
-db.setUserName("fatmal");//inserer nom de l'utilisateur
-db.setPassword("esprit20");//inserer mot de passe de cet utilisateur
+db.setUserName("mariem");//inserer nom de l'utilisateur
+db.setPassword("esprit17");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
